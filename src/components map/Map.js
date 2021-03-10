35035bfa46d971ab434f1fbbc03e3940c53d1ba6
Ahmet Batuhan Kılıç <han_batur@hotmail.com>
2021-03-10 15:24:13 +0300

    import Footer component

--- src/components map/Map.js
+++ src/components map/Map.js
@@ -2,3 +2,4 @@
 export { default as Homepage } from "../components/Homepage"
 /* Components */
 export { default as Header } from "../components/Header"
+export { default as Footer } from "../components/Footer"
