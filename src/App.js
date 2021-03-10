f8fc2620e26f5dedf6a77de19f452544d04007d5
Ahmet Batuhan Kılıç <han_batur@hotmail.com>
2021-03-10 15:18:47 +0300

    update import react router dom

--- src/App.js
+++ src/App.js
@@ -1,9 +1,25 @@
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route
+} from "react-router-dom"
+
 import { Homepage } from './components map/Map'
 
 function App() {
   return (
     <div className="App">
-      <Homepage />
+      <Router>
+        <Header />
+        <main>
+          <div className="">
+            <Switch>
+              <Route path="/" component={Homepage} />
+            </Switch>
+          </div>
+        </main>
+        <Footer />
+      </Router >
     </div>
   );
 }
