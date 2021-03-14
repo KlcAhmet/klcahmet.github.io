1955615580a4c9e7332e30454d31b3a407310b2e
Ahmet Batuhan Kılıç <han_batur@hotmail.com>
2021-03-15 02:07:53 +0300

    import particles's css folders

--- src/App.js
+++ src/App.js
@@ -7,16 +7,20 @@ import {
 import { Homepage, Header, Footer } from './components map/Map'
 /* CSS */
 import 'bootstrap/dist/css/bootstrap.css'
+import './css/particles.css'
+import './css/particle.css'
+
 
 function App() {
+
   return (
     <div className="App">
       <Router>
-        <div className="row">
+        <div className="row row--margin">
           <div className="col-1">
             <Header />
           </div>
-          <div className="col-11">
+          <div className="col-11 col--padding">
             <main>
               <div className="">
                 <Switch>
