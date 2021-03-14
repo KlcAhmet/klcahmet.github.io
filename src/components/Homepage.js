9cfade94fa9a39e2eb9b23220ef134dd21bf9211
Ahmet Batuhan Kılıç <han_batur@hotmail.com>
2021-03-15 02:08:46 +0300

    update test içerik yazıldı

--- src/components/Homepage.js
+++ src/components/Homepage.js
@@ -1,6 +1,16 @@
+import Particle from "./Particle"
+
 function Homepage() {
     return (
-        <p>test homepage</p>
+        <div className="home">
+            <div className="particle">
+                <Particle />
+            </div>
+            <div className="welcome">
+                <h1 className="welcome__title">Ahmet Batuhan Kılıç</h1>
+                <p className="welcome__subtitle">Front-end Developer</p>
+            </div>
+        </div>
     )
 }
 
