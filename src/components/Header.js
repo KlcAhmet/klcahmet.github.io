24f7b0838718e53238561199ac88634820e907ee
Ahmet Batuhan Kılıç <han_batur@hotmail.com>
2021-03-16 02:04:09 +0300

    update Header linkler eklendi

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,7 +1,43 @@
+
+
 function Header() {
     return (
-        <div>
-            <p>header</p>
+        <div className="navBar">
+            <div className="navBar__burger">
+                <p>|||</p>
+            </div>
+            <ul>
+                <li>
+                    <a href="#hakkımda" role="button" >
+                        <span>H</span>
+                        <span className="hidden">akkımda</span>
+                    </a>
+                </li>
+                <li>
+                    <a href="#projeler" role="button">
+                        <span>P</span>
+                        <span className="hidden">rojeler</span>
+                    </a>
+                </li>
+                <li>
+                    <a href="#yetenekler" role="button">
+                        <span>Y</span>
+                        <span className="hidden">etenekler</span>
+                    </a>
+                </li>
+                <li>
+                    <a href="#cv" role="button">
+                        <span>C</span>
+                        <span className="hidden">V</span>
+                    </a>
+                </li>
+                <li>
+                    <a href="#iletisim" role="button">
+                        <span>İ</span>
+                        <span className="hidden">letişim</span>
+                    </a>
+                </li>
+            </ul>
         </div>
     )
 }
