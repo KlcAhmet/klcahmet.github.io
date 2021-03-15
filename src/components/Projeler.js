00a8f6490af8d156312d2ccc8f6c75540675cbc8
Ahmet Batuhan Kılıç <han_batur@hotmail.com>
2021-03-16 02:35:56 +0300

    create Projeler.js

---
+++ src/components/Projeler.js
@@ -0,0 +1,12 @@
+const Projeler = () => {
+    return (
+        < div className="row d-flex justify-content-center" >
+            <div className="bilgi col-7">
+                <h3 className="bilgi_title">Projelerim</h3>
+                <p className="bilgi_paragraph">Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempora esse asperiores cumque corrupti, ex, eos itaque necessitatibus sequi perspiciatis ad harum numquam nam, magni totam atque earum odio amet ratione.</p>
+            </div>
+        </div >
+    )
+}
+
+export default Projeler
