007057d32b55a8d25ef45e2af8ab5981e5f8ffdd
Ahmet Batuhan Kılıç <han_batur@hotmail.com>
2021-03-16 02:18:02 +0300

    create Hakkımda.js

---
+++ src/components/Hakkımda.js
@@ -0,0 +1,11 @@
+const Hakkımda = () => {
+    return (
+        < div className="row d-flex justify-content-center" >
+            <div className="col-5">
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempora esse asperiores cumque corrupti, ex, eos itaque necessitatibus sequi perspiciatis ad harum numquam nam, magni totam atque earum odio amet ratione.</p>
+            </div>
+        </div >
+    )
+}
+
+export default Hakkımda
