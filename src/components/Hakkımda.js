eeaf4d2911d25d6be7d9622f64369b9c9ecd95fe
Ahmet Batuhan Kılıç <han_batur@hotmail.com>
2021-03-16 16:49:05 +0300

    upload hakkımda ve projeler kısmı güncellendi

--- src/components/Hakkımda.js
+++ src/components/Hakkımda.js
@@ -3,7 +3,14 @@ const Hakkımda = () => {
         < div className="row row-yazı--margin" >
             <div className="bilgi">
                 <h3 className="bilgi__title">Hakkımda</h3>
-                <p className="bilgi__paragraph bilgi__paragraph--gray">Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempora esse asperiores cumque corrupti, ex, eos itaque necessitatibus sequi perspiciatis ad harum numquam nam, magni totam atque earum odio amet ratione.</p>
+                <p className="bilgi__paragraph bilgi__paragraph--gray">
+                    Merhaba. Ben Ahmet Batuhan Kılıç. Bilgisayar Mühendisliği okuyorum. Front-end developer olarak kariyerime
+                    devam etmekteyim. Freelance web geliştiriyorum. Mühendislik hayatım Konya'da Kto Karatay Üniversitesinde başladı ve Düzce Üniversitesine yatay geçiş yaptım.
+                    Zamanımın büyük bir kısmını kariyerim için çalışarak geçiriyorum. Kalan zamanlarımda hobilerim olarak satranç turnuvalarına katılıyorum,
+                    bisikletim ile orman yollarında veya downhill MTB sürüşleri yapıyorum. Sosyal olarak Konya'da bulunduğum dönemlerde Sivil Yaşam Derneği üyesiydim ve
+                    bir çok organizasyonunda görev aldım. Şuan Best Buddies International'ın Türkiye organizasyonu olan Best Buddies Türkiye organizasyonun E-Buddies
+                    yazılımında gönüllü olarak Front-end yazılımcı olarak görev almaktayım.
+                </p>
             </div>
         </div >
     )
--- src/components/Projeler.js
+++ src/components/Projeler.js
@@ -4,7 +4,11 @@ const Projeler = () => {
             <div className="bilgi--divColor bilgi--padding">
                 <div className="bilgi">
                     <h3 className="bilgi__title bilgi__title--white">Projelerim</h3>
-                    <p className="bilgi__paragraph bilgi__paragraph--white">Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempora esse asperiores cumque corrupti, ex, eos itaque necessitatibus sequi perspiciatis ad harum numquam nam, magni totam atque earum odio amet ratione.</p>
+                    <p className="bilgi__paragraph bilgi__paragraph--white">
+                        Web üzerine projeler geliştiriyorum. Her projemde öğrendiklerimi bir sonraki projemde daha performanslı ve usefull geliştiriyorum.
+                        Geliştirmelerimi stabil çalışan en güncel teknolojiler ile yürütmeyi tercih ediyorum. Küçük ölçekli yazılımlarda yeteneklerimi üst seviyelere
+                        taşıyorum ve şuan Sakarya Üniversitesi Teknokentinde bulunan FINTECH şirketinden sektöre adaptasyon sürecim için eğitim üzerine projeler yapıyorum.
+                    </p>
                 </div>
             </div>
         </div >
