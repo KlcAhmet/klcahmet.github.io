3a17e0cf6cfbcc0fd01ba869d19407dbb536b9d3
Ahmet Batuhan Kılıç <han_batur@hotmail.com>
2021-03-16 03:12:10 +0300

    update className düzeltildi

--- src/components/Hakkımda.js
+++ src/components/Hakkımda.js
@@ -2,8 +2,8 @@ const Hakkımda = () => {
     return (
         < div className="row d-flex justify-content-center" >
             <div className="bilgi col-7">
-                <h3 className="bilgi_title">Hakkımda</h3>
-                <p className="bilgi_paragraph">Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempora esse asperiores cumque corrupti, ex, eos itaque necessitatibus sequi perspiciatis ad harum numquam nam, magni totam atque earum odio amet ratione.</p>
+                <h3 className="bilgi__title">Hakkımda</h3>
+                <p className="bilgi__paragraph bilgi__paragraph--gray">Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempora esse asperiores cumque corrupti, ex, eos itaque necessitatibus sequi perspiciatis ad harum numquam nam, magni totam atque earum odio amet ratione.</p>
             </div>
         </div >
     )
--- src/components/Projeler.js
+++ src/components/Projeler.js
@@ -1,9 +1,11 @@
 const Projeler = () => {
     return (
-        < div className="row d-flex justify-content-center" >
-            <div className="bilgi col-7">
-                <h3 className="bilgi_title">Projelerim</h3>
-                <p className="bilgi_paragraph">Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempora esse asperiores cumque corrupti, ex, eos itaque necessitatibus sequi perspiciatis ad harum numquam nam, magni totam atque earum odio amet ratione.</p>
+        < div className="row" >
+            <div className="col-12 bilgi--divColor d-flex justify-content-center">
+                <div className="bilgi col-7">
+                    <h3 className="bilgi__title bilgi__title--white">Projelerim</h3>
+                    <p className="bilgi__paragraph bilgi__paragraph--white">Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempora esse asperiores cumque corrupti, ex, eos itaque necessitatibus sequi perspiciatis ad harum numquam nam, magni totam atque earum odio amet ratione.</p>
+                </div>
             </div>
         </div >
     )
