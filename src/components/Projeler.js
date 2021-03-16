2643e041036561d330b959e675f52a4d65f09caf
Ahmet Batuhan Kılıç <han_batur@hotmail.com>
2021-03-16 03:33:51 +0300

    update padding eklendi

--- src/components/Projeler.js
+++ src/components/Projeler.js
@@ -1,7 +1,7 @@
 const Projeler = () => {
     return (
         < div className="row row-yazı--margin" >
-            <div className="bilgi--divColor">
+            <div className="bilgi--divColor bilgi--padding">
                 <div className="bilgi">
                     <h3 className="bilgi__title bilgi__title--white">Projelerim</h3>
                     <p className="bilgi__paragraph bilgi__paragraph--white">Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempora esse asperiores cumque corrupti, ex, eos itaque necessitatibus sequi perspiciatis ad harum numquam nam, magni totam atque earum odio amet ratione.</p>
