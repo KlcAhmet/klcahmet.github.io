799ee94645f0dadc13eccb68c2fd9a1d828a3deb
Ahmet Batuhan Kılıç <han_batur@hotmail.com>
2021-03-16 03:30:32 +0300

    update responsive sorunu giderildi

--- src/components/Hakkımda.js
+++ src/components/Hakkımda.js
@@ -1,7 +1,7 @@
 const Hakkımda = () => {
     return (
-        < div className="row row-yazı--margin d-flex justify-content-center" >
-            <div className="bilgi col-7">
+        < div className="row row-yazı--margin" >
+            <div className="bilgi">
                 <h3 className="bilgi__title">Hakkımda</h3>
                 <p className="bilgi__paragraph bilgi__paragraph--gray">Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempora esse asperiores cumque corrupti, ex, eos itaque necessitatibus sequi perspiciatis ad harum numquam nam, magni totam atque earum odio amet ratione.</p>
             </div>
--- src/components/Projeler.js
+++ src/components/Projeler.js
@@ -1,8 +1,8 @@
 const Projeler = () => {
     return (
         < div className="row row-yazı--margin" >
-            <div className="col-12 bilgi--divColor d-flex justify-content-center">
-                <div className="bilgi col-7">
+            <div className="bilgi--divColor">
+                <div className="bilgi">
                     <h3 className="bilgi__title bilgi__title--white">Projelerim</h3>
                     <p className="bilgi__paragraph bilgi__paragraph--white">Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempora esse asperiores cumque corrupti, ex, eos itaque necessitatibus sequi perspiciatis ad harum numquam nam, magni totam atque earum odio amet ratione.</p>
                 </div>
