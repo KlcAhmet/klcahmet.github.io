62d3f4b10c835dd98f877617e103ab36e2ce7e1f
Ahmet Batuhan Kılıç <han_batur@hotmail.com>
2021-04-02 03:57:49 +0300

    update

--- src/components/Homepage.js
+++ src/components/Homepage.js
@@ -1,18 +1,30 @@
 import Particle from "./Particle"
 import { Col } from 'react-bootstrap';
 import { Startpage } from '../components map/Map'
+import Github from '../img/Github.svg'
+import Envelope from '../img/Envelope.svg'
+import Google from '../img/Google.svg'
+import Instagram from '../img/Instagram.svg'
+import Linkedin from '../img/Linkedin.svg'
 
 function Homepage() {
     return (
         <div className="home">
+            <Startpage />
             <div className="particle">
                 <Particle />
             </div>
             <Col className="welcome" xs={12}>
-                <h1 className="welcome__title">Ahmet Batuhan Kılıç</h1>
-                <p className="welcome__subtitle">Front-end Developer</p>
+                <h1 className="welcome-title">Ahmet Batuhan Kılıç</h1>
+                <p className="welcome-subtitle">Front-end Developer</p>
+                <div className="welcome-socialmedia">
+                    <a href="https://github.com/KlcAhmet" target="_blank"><img className="welcome-socialmedia-github" src={Github} alt="" /></a>
+                    <a href="https://www.linkedin.com/in/ahmet-batuhan-kılıç-b88975129/" target="_blank"><img className="welcome-socialmedia-linkedin" src={Linkedin} alt="" /></a>
+                    <a href="https://www.instagram.com/ahmetbatukilic/" target="_blank"><img className="welcome-socialmedia-ins" src={Instagram} alt="" /></a>
+                    <a href="https://g.page/AhmetBatuhanKilic?share" target="_blank"><img className="welcome-socialmedia-google" src={Google} alt="" /></a>
+                    <a href="mailto:ahmetbatukilic@gmail.com"><img className="welcome-socialmedia-env" src={Envelope} alt="" /></a>
+                </div>
             </Col>
-            <Startpage />
         </div>
     )
 }
--- src/components/Startpage.js
+++ src/components/Startpage.js
@@ -1,10 +1,28 @@
 import { Col, Row } from 'react-bootstrap';
+import eric1 from '../img/eric1.png'
+import eric2 from '../img/eric2.png'
+import eric3 from '../img/eric3.png'
 
 const Startpage = () => {
+    const hidden = () => {
+        setTimeout(function () {
+            document.getElementById('startpage').classList.add("hidden")
+        }, 9000);
+    }
+
+
     return (
-        <Row className="startpage" style={{ padding: 0 }}>
-            <Col className="startpage-up" xs={12}>lor</Col>
-            <Col className="startpage-down" xs={12}>lor</Col>
+        <Row id="startpage" className="startpage" style={{ padding: 0 }} onLoad={hidden}>
+            <Col className="startpage-up" xs={12}>
+                <div className="eric">
+                    <Row>
+                        <Col xs={4}><img className="eric-img eric-first img-fluid" src={eric1} alt="" /></Col>
+                        <Col xs={4}><img className="eric-img eric-second  img-fluid" src={eric2} alt="" /></Col>
+                        <Col xs={4}><img className="eric-img eric-third img-fluid" src={eric3} alt="" /></Col>
+                    </Row>
+                </div>
+            </Col>
+            <Col className="startpage-down" xs={12}></Col>
         </Row>
     )
 }
